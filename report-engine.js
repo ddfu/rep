3871da8f0210856293e3d3308ac59b1faac5ea90
Chris <chris@Chriss-Mac-mini.lan>
2014-11-02 05:00:13 +0800

    Splitting works

--- report-engine.js
+++ report-engine.js
@@ -6,13 +6,17 @@ var rwEngine = new (function(){
 	
 	/* Pre-fetch */
 	var templates = {};
-	$.get( "components/report-body.html", function(d){ templates.report=d } );
+	$.get( "components/report-body.html", 		function(d){ templates.report=d  } );
+	$.get( "components/report-pivoter.html", 	function(d){ templates.pivoter=d } );
+	
+	var reportDefns = {};
+	$.get( "report-defns.txt", function(d){ reportDefns=JSON.parse(d) } );
 	
 	/* Report class */
 	var Report = function( type, div )
 	{
 		/* Connect report template to our div */
-		var sel = $( div ).find( ".navbar-brand" );
+		var sel = div.find( ".rw-report-name" );
 		
 		if( type == "demo" )
 			sel.html( "Demo Report" );
@@ -20,6 +24,101 @@ var rwEngine = new (function(){
 			sel.html( "Griffin Report" );
 		else
 			sel.html( type + " Report" );
+			
+		/* Provide access to div */
+		this.div = div;
+		
+		/* Put the trailing report in its own row */
+		this.resize = function()
+		{
+			var pos = -1;
+			
+			for( var i = 0; i < reports.length; ++i )
+				if( reports[ i ] == this )
+					pos = i;
+					
+			if( pos % 2 == 0 && pos == reports.length - 1 )
+				div.attr( "class", "col-sm-12 col-md-12 col-lg-12" );
+			else
+				div.attr( "class", "col-sm-6 col-md-6 col-lg-6" );
+		}
+		
+		/* First-time draw of report (post-configuration) */
+		var draw = function()
+		{
+			div.find( ".rw-report-progress" ).remove();
+			div.find( ".rw-report-body" ).html( templates.pivoter );
+		};
+		
+		/* Download data for report */
+		var load = function()
+		{
+			div.find( ".rw-report-configure" ).remove();
+			div.find( ".rw-report-progress" ).html( "<div>Loading data...</div><div class=\"progress\">\
+			  <div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"40\" \
+			  	aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 40%\">\
+			  </div>\
+			</div>" );
+			
+			// fake it up
+			window.setTimeout( draw, 500 );
+		};
+		
+		this.load = load;
+		
+		/* Recursive setup menu generation - although not recursive yet... */
+		var menu = reportDefns[ type ].menu;
+		var menuRootDiv = div.find( ".rw-report-configure" ).first();
+		var menuState = []; // unused
+		
+		var drawMenuInner = function( div, menu, state )
+		{
+			div.append( $( "<h4>Configure</h4>" ) );
+			
+			for( var e = 0; e < menu.length; ++e )
+			{
+				switch( menu[e].type )
+				{
+				case "dropdown":
+					div.append( $( "<span>" + menu[e].name + ": </span> " ) );
+					var select = document.createElement( "select" );
+					
+					for( var k in menu[e].choices )
+						$( select ).append( $( "<option>"+menu[e].choices[k]+"</option>" ) );
+						
+					div.append( select );
+					div.append( $( "<br />" ) );
+					break;
+					
+				case "check":
+					div.append( $( "<input/>", { type: "checkbox", name: menu[e].name, checked: 1 } ) );
+					div.append( $( "<span/>", { text: " " + menu[e].name } ) );
+					div.append( $( "<br />" ) );
+					break;
+					
+				case "text":
+					div.append( $( "<input/>", { type: "text", value: "test" } ) );
+					div.append( $( "<br />" ) );
+					break;
+					
+				case "label":
+					div.append( $( "<span/>", { text: menu[e].text } ) );
+					div.append( $( "<br />" ) );
+					break;
+				}
+			}
+			
+			div.append( $( "<br/>" ) );
+			div.append( $( "<button/>", { type: "button", class: "btn btn-primary", text: "Generate",
+										  click: load } ) );
+		};
+		
+		this.drawMenu = function()
+		{
+			drawMenuInner( menuRootDiv, menu );	
+		};
+		
+		this.drawMenu();
 	};
 	
 	this.addReport = function( type )
@@ -36,5 +135,8 @@ var rwEngine = new (function(){
 		
 		var newReport = new Report( type, elmt );
 		reports.push( newReport );
+		
+		for( var i = 0; i < reports.length; ++i )
+			reports[ i ].resize();
 	};
 });
